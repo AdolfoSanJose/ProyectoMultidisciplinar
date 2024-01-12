6df3fe1adf179041215f291799bdafad07075ff9
AdolfoSanJose <adolfoperez-gasconv.sanjosemlg@fundacionloyola.net>
2024-01-12 01:16:06 +0100

    Commit de mi feature, Register sin terminar aún.

--- hospitapp/lib/components/my_textfield.dart
+++ hospitapp/lib/components/my_textfield.dart
@@ -4,21 +4,27 @@ class MyTextField extends StatelessWidget {
   final controller;
   final String hintText;
   final bool obscureText;
+  final String errorText;
 
-  const MyTextField({
-    super.key,
-    required this.controller,
-    required this.hintText,
-    required this.obscureText,
-  });
+  const MyTextField(
+      {super.key,
+      required this.controller,
+      required this.hintText,
+      required this.obscureText,
+      required this.errorText});
 
   @override
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.symmetric(horizontal: 25.0),
-      child: TextField(
+      child: TextFormField(
         controller: controller,
         obscureText: obscureText,
+        validator: (value) {
+          if (value!.isEmpty) {
+            return errorText;
+          }
+        },
         decoration: InputDecoration(
           enabledBorder: const OutlineInputBorder(
             borderSide: BorderSide(color: Colors.white),
@@ -26,7 +32,7 @@ class MyTextField extends StatelessWidget {
           focusedBorder: const OutlineInputBorder(
             borderSide: BorderSide(color: Colors.black),
           ),
-          fillColor: Color.fromARGB(255, 146, 189, 255),
+          fillColor: const Color.fromARGB(255, 146, 189, 255),
           filled: true,
           hintText: hintText,
         ),
